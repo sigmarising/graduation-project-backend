95a705906e419174ebdcee3d27e7ade54080cc82
Jason Zhang <sigmarising@hotmail.com>
2019-04-26 23:24:55 +0800

    ADD: more REST api for color imagery season solarTerm

--- serverFlask.py
+++ serverFlask.py
@@ -26,6 +26,50 @@ def api_v1_char_summary():
         abort(400)
 
 
+@app.route('/api/v1/seasonSummary', methods=['GET', 'PUT', 'POST', 'DELETE'])
+def api_v1_season_summary():
+    if request.method == 'GET':
+        season_path = os.path.abspath(os.path.join(STATIC_PATH, 'data/season.json'))
+        with open(season_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        return jsonify(data)
+    else:
+        abort(400)
+
+
+@app.route('/api/v1/solarTermSummary', methods=['GET', 'PUT', 'POST', 'DELETE'])
+def api_v1_solar_term_summary():
+    if request.method == 'GET':
+        solar_term_path = os.path.abspath(os.path.join(STATIC_PATH, 'data/solar_term.json'))
+        with open(solar_term_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        return jsonify(data)
+    else:
+        abort(400)
+
+
+@app.route('/api/v1/imagerySummary', methods=['GET', 'PUT', 'POST', 'DELETE'])
+def api_v1_imagery_summary():
+    if request.method == 'GET':
+        imagery_path = os.path.abspath(os.path.join(STATIC_PATH, 'data/imagery.json'))
+        with open(imagery_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        return jsonify(data)
+    else:
+        abort(400)
+
+
+@app.route('/api/v1/colorSummary', methods=['GET', 'PUT', 'POST', 'DELETE'])
+def api_v1_color_summary():
+    if request.method == 'GET':
+        color_path = os.path.abspath(os.path.join(STATIC_PATH, 'data/color.json'))
+        with open(color_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        return jsonify(data)
+    else:
+        abort(400)
+
+
 if __name__ == '__main__':
     print(STATIC_PATH)
     app.run(debug=settings.DEBUG)
